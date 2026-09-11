be328060e701970e914edd5d1e480d37f9011c78
mindtrap110 <mindtrap110@gmail.com>
2026-09-12 01:14:08 +0900

    Refresh PWA cache for interval analysis

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vocal-scope-v2-20260909-pitchcontrol1';
+const CACHE = 'vocal-scope-v2-20260912-interval1';
 const ASSETS = ['./','./index.html','./styles.css','./history.css','./app.js','./history.js','./pitch-core.js','./pitch-worker.js','./manifest.webmanifest','./icon-180.png','./icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
